93e29686829c160230261d34b9a3eca5c5d64bfa
DevinAhmad <f.devin.ahmad@gmail.com>
2018-08-01 14:17:09 +0700

    Update 0.06

--- Face_Count.py
+++ Face_Count.py
@@ -1,4 +1,4 @@
-#version 0.05b
+#version 0.06
 '''
 #log 0.02: tambahan write/append data total muka yang terdeteksi per menit dalam notepad
 #log 0.04: revisi sistem detik/waktu yang benar
@@ -8,6 +8,7 @@ sistem interval dan delay pengecekan muka diubah, sekarang menggunakan detik, bu
 #log 0.05: setting parameter detectMultiScale untuk mengatur sensitivitas dan akurasi deteksi muka
 write durasi proses, durasi video, dan parameter detectMultiScale
 #log 0.05b: tambah variabel dan write interval dan delay
+#log 0.06: z counter untuk menghitung jumlah muka yang terdeteksi digantikan numDetect
 '''
 
 import cv2
@@ -18,10 +19,10 @@ import time
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
-
-cap = cv2.VideoCapture('bandicam_1.mp4')
+captMedia=0
+cap = cv2.VideoCapture(captMedia)
 fps = 17.25
-z = 0
+
 z0 = 0
 z1 = 0
 z2 = 0
@@ -41,9 +42,9 @@ start=time.time()
 #parameter detectMultiScale2
 #scaleFactor 1.05 - 1.4, less=better, detect less, slower (minimum=1.05)
 #minNeighbors 3 - 6, less= detect more, less accuracy
-#minSize (50, 50) for faces, limit minimum size that can be detected
+#minSize (50, 50) for faces, limit minimum size that can be detected, less=more processing time
 scaleFactor = 1.05
-minNeighbors= 3
+minNeighbors= 12
 minSize=	  (90, 90)
 
 #setting interval dan delay
@@ -54,11 +55,13 @@ while True:
     #deteksi muka dengan cascade
     ret, img = cap.read()
     if not ret:
-      print('Durasi proses:', time.time()-start, ' detik')
+      detik=int(detik)
+      durasi = time.time()-start
+      print('Durasi proses: %.4f' %durasi, ' detik')
       print('Durasi video:', menit,' menit', detik, ' detik')
       f= open("test.txt","a+")
       f.write('\r\n')
-      f.write('Durasi proses: '+ str(time.time()-start)+ ' detik'+ '\r\n')
+      f.write('Durasi proses: %.4f' %durasi + ' detik'+ '\r\n')
       f.write('Durasi video: '+ str(menit)+ ' menit '+ str(detik)+ ' detik '+ '\r\n')
       f.write('scaleFactor= '+ str(scaleFactor)+ '\r\n'+ 'minNeighbors= '+ str(minNeighbors)+ '\r\n'+ 'minSize= '+ str(minSize)+'\r\n')
       f.write('interval= '+ str(minInterval)+ '\r\n'+ 'delay= '+ str(minDelay))
@@ -67,12 +70,12 @@ while True:
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces, numDetect = face_cascade.detectMultiScale2(gray, scaleFactor=scaleFactor, minNeighbors=minNeighbors, minSize=minSize)
-    font = cv2.FONT_HERSHEY_SIMPLEX
+    numDetect=len(numDetect)
     
     #gambar kotak dan counter untuk muka    
     for (x,y,w,h) in faces:
        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
-       z = z + 1
+       
        
     #menghitung detik asli video
     counter=counter + 1
@@ -83,7 +86,7 @@ while True:
     interval = counter2/fps
 
     #delay bila tidak ada muka yang terdeteksi
-    if z==0:
+    if numDetect==0:
       counter3 = counter3 + 1
       delay = counter3/fps
       if delay >= minDelay:
@@ -91,15 +94,15 @@ while True:
         z0=0
     
     #bila muka terdeteksi
-    if z>=1:
+    if numDetect>=1:
       delay=0
       if interval>=minInterval:
         counter2=0
         counter3=0
-        delta = z0-z
-        if z0<=z:
+        delta = z0-numDetect
+        if z0<=numDetect:
           z2= z2 + abs(delta)
-        z0=z
+        z0=numDetect
      
 
     '''
@@ -108,13 +111,6 @@ while True:
         detik = time.time() - start
         print ("time", detik)
     '''
-
-
-
-
-
-    
-    
 
     #print total orang per __ detik
     if detik >=60:
@@ -128,26 +124,27 @@ while True:
       z0=0
       counter=0
     
-    #cv2.putText(img,str(numDetect)+'s',(10,20), font, 0.5,(255,0,0),1,cv2.LINE_AA)
-    
-    detik=int(detik)
-    cv2.putText(img,str(interval)+'s',(10,20), font, 0.5,(255,0,0),1,cv2.LINE_AA)
-    cv2.putText(img,'Delta: '+str(abs(delta)),(10,40), font, 0.5,(255,0,0),1,cv2.LINE_AA)
-    cv2.putText(img,'Person: '+str(z),(10,60), font, 0.5,(255,0,0),1,cv2.LINE_AA)
-    cv2.putText(img,'Total: '+str(z2),(10,80), font, 0.5,(255,0,0),1,cv2.LINE_AA)
-    cv2.putText(img,'Delay: '+str(delay),(10,100), font, 0.5,(255,0,0),1,cv2.LINE_AA)
-    cv2.putText(img,'Detik: '+str(detik),(10,120), font, 0.5,(255,0,0),1,cv2.LINE_AA)
-    cv2.putText(img,'z0: '+str(z0),(10,140), font, 0.5,(255,0,0),1,cv2.LINE_AA)
-    
-
-    #reset counter pendeteksi muka 
-    z=0
+    if captMedia==0:
+      font = cv2.FONT_HERSHEY_SIMPLEX
+      #cv2.putText(img,str(numDetect),(10,160), font, 0.5,(255,0,0),1,cv2.LINE_AA)
+      
+      detik=int(detik)
+      cv2.putText(img,str(interval)+'s',(10,20), font, 0.5,(255,0,0),1,cv2.LINE_AA)
+      cv2.putText(img,'Delta: '+str(abs(delta)),(10,40), font, 0.5,(255,0,0),1,cv2.LINE_AA)
+      cv2.putText(img,'Person: '+str(numDetect),(10,60), font, 0.5,(255,0,0),1,cv2.LINE_AA)
+      cv2.putText(img,'Total: '+str(z2),(10,80), font, 0.5,(255,0,0),1,cv2.LINE_AA)
+      cv2.putText(img,'Delay: '+str(delay),(10,100), font, 0.5,(255,0,0),1,cv2.LINE_AA)
+      cv2.putText(img,'Detik: '+str(detik),(10,120), font, 0.5,(255,0,0),1,cv2.LINE_AA)
+      cv2.putText(img,'z0: '+str(z0),(10,140), font, 0.5,(255,0,0),1,cv2.LINE_AA)
+      cv2.imshow('img',img)
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+ 
 
     #menunjukan window video(25, 33, 57) 
-    #cv2.imshow('img',img)
-    #if cv2.waitKey(1) & 0xFF == ord('q'):
-    # break
- 
+      
+      
+        
 cap.release()
 cv2.destroyAllWindows
                                      
